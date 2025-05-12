44ab898558e179eb576423e5215348aa34897b06
Ahmadjon2160 <ahmadjonmt2160@gmail.com>
2025-05-12 21:09:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let random1 = (Math.random() * 100).toFixed();
+
+console.log(random1);
+
+alert(random1 * 2 + " =" + random1 + " * 2=  random sonni  ikkiga kpaytirganda");
+alert(random1 / 2 + "=" + random1 + " / 2=  random sonni ikki ga boling");
